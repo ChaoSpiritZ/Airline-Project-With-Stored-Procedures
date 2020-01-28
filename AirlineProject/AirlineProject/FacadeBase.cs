cf80111b2e68149a0779a1f87e5fa5bce641d61a
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-01-28 18:05:28 +0200

    Add files via upload

---
+++ AirlineProject/AirlineProject/FacadeBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineProject
+{
+    public abstract class FacadeBase
+    {
+        protected AirlineDAOMSSQL _airlineDAO;
+        protected CountryDAOMSSQL _countryDAO;
+        protected CustomerDAOMSSQL _customerDAO;
+        protected FlightDAOMSSQL _flightDAO;
+        protected TicketDAOMSSQL _ticketDAO;
+        
+        public FacadeBase()
+        {
+            _ticketDAO = new TicketDAOMSSQL();
+            _customerDAO = new CustomerDAOMSSQL();
+            _flightDAO = new FlightDAOMSSQL();
+            _countryDAO = new CountryDAOMSSQL();
+            _airlineDAO = new AirlineDAOMSSQL();
+        }
+    }
+}
